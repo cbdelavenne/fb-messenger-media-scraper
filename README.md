2603ca6ac01762226593e945df7c9d826c7f4c22
Asim (Cyrille) B. Delavenne <asimdlv@cigotracker.com>
2019-08-28 21:53:26 -0400

    Added Threads and Messages search before_date support for simple date Y-m-d, and updated related doc

--- README.md
+++ README.md
@@ -12,7 +12,11 @@ Use `config.ini.example` as a reference guide.
 - `[Credentials][password]`: Your FB account password used for authentication.
 - `[Friend][url]`: The URL to your friend's FB page.
 - `[Threads][search_limit]`: Number of threads to look through (from most recent). If the conversation you're looking for is very old, you'll have to increase this number. Otherwise, you'll want to set this to a low value for speed.
+- `[Threads][before_date]`: (**Note:** Optional. If not used, leave value after `=` blank) Specify a date (in format Y-m-d, i.e. 2019-08-15) from which point to retrieve threads. 
 - `[Messages][search_limit]`: Number of messages to search through for images once the thread with your friend has been found. Again, if the media you want to download is hundreds of messages go, you'll want to increase this value proportionally.
+- `[Messages][before_date]`: (**Note:** Optional. If not used, leave value after `=` blank) Specify a date (in format Y-m-d, i.e. 2019-08-15) from which point to retrieve messages. For instance, for a given thread, the script will only download images sent in last `[Messages][search_limit]` starting from the date specified here.
+ 
+- `[Media][sender_only]`: Simple `true` or `false` value. If `true`, only images from your target Friend will be downloaded. 
 - `[Download][path]`: Local target path for the images to be downloaded to.
 
 ### Execution
--- fbm-scraper.py
+++ fbm-scraper.py
@@ -3,10 +3,14 @@ import requests
 import time
 import uuid
 import configparser
+import datetime
 
 from fbchat import Client, ImageAttachment
 from fbchat import FBchatException
 from pathlib import Path
+
+politeness_index = 0.5  # ;)
+epoch = datetime.datetime(1970, 1, 1)
 
 
 def download_file_from_url(url, target_path):
@@ -15,12 +19,33 @@ def download_file_from_url(url, target_path):
 
     :param url: URL of file to download
     :param target_path: Local target path to save the file
+    :type url: str
+    :type target_path: str
     """
     if url is not None:
         r = requests.get(url)
         with open(target_path, 'wb') as f:
             print('\tDownloading image to {path}'.format(path=target_path))
             f.write(r.content)
+
+
+def convert_date_to_unix_ms(date, as_int=True):
+    """
+    Convert a given date string to epoch (int in milliseconds)
+
+    :param date: Date string (preferred format %Y-%m-%d)
+    :param as_int: Return unix timestamp as an integer value, instead of a float
+    :type date: str
+    :type as_int: int
+    :return: int
+    """
+    try:
+        dt = datetime.datetime.strptime(date, '%Y-%m-%d')
+        res = ((dt - epoch).total_seconds() * 1000)  # convert to milliseconds
+
+        return int(res) if as_int else res
+    except ValueError:
+        return None
 
 
 if __name__ == '__main__':
@@ -32,34 +57,44 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read(config_path)
 
-    download_path = config['Download']['path']
+    download_path = config.get('Download', 'path')
     if os.path.exists(download_path) is False:
         raise Exception("The path specified in download_path does not exist ({path}). Please specify a valid path in "
                         "config.ini".format(path=download_path))
 
     # Initialize FB Client
-    fb_email = config['Credentials']['email']
-    fb_pw = config['Credentials']['password']
+    fb_email = config.get('Credentials', 'email')
+    fb_pw = config.get('Credentials', 'password')
     fb_client = Client(fb_email, fb_pw)
 
     # Search for latest threads
-    thread_search_limit = int(config['Threads']['search_limit'])
-    threads = fb_client.fetchThreadList(limit=thread_search_limit)
+    thread_search_limit = int(config.get('Threads', 'search_limit'))
+    thread_search_before = convert_date_to_unix_ms(config.get('Threads', 'before_date'))
+
+    if thread_search_before is not None:
+        threads = fb_client.fetchThreadList(limit=thread_search_limit, before=thread_search_before)
+    else:
+        threads = fb_client.fetchThreadList(limit=thread_search_limit)
 
     # Find correct thread for given user URL
     my_thread = None
     for thread in threads:
-        if thread.url == config['Friend']['url']:
+        if thread.url == config.get('Friend', 'url'):
             my_thread = thread
 
     # Get Messages for my_thread
     if my_thread is not None:
-        message_search_limit = int(config['Messages']['search_limit'])
-        messages = fb_client.fetchThreadMessages(my_thread.uid, limit=message_search_limit)
+        message_search_limit = int(config.get('Messages', 'search_limit'))
+        message_search_before = convert_date_to_unix_ms(config.get('Messages', 'before_date'))
 
-        full_images = []
+        if message_search_before is not None:
+            messages = fb_client.fetchThreadMessages(my_thread.uid, limit=message_search_limit,
+                                                     before=message_search_before)
+        else:
+            messages = fb_client.fetchThreadMessages(my_thread.uid, limit=message_search_limit)
 
         # Extract Image attachments' full-sized image signed URLs (along with their original file extension)
+        full_images = []
         for message in messages:
             if len(message.attachments) > 0:
                 for attachment in message.attachments:
@@ -89,7 +124,7 @@ if __name__ == '__main__':
                 download_file_from_url(img_url, image_path)
 
                 # Sleep half a second between file downloads to avoid getting flagged as a bot
-                time.sleep(0.5)
+                time.sleep(politeness_index)
         else:
             print('No images to download in the last {count} messages'.format(count=message_search_limit))
     else:
